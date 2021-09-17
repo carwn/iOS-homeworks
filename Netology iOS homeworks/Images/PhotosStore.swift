ec436d3272028a3d57aac0bad060047408d06daa
Александр Шелихов <carwn@bk.ru>
2021-09-17 10:19:11 +0700

    Добавил 20 фото и создал пустой PhotosTableViewCell.

---
+++ Netology iOS homeworks/Images/PhotosStore.swift
@@ -0,0 +1,31 @@
+//
+//  PhotosStore.swift
+//  Netology iOS homeworks
+//
+//  Created by Александр Шелихов on 17.09.2021.
+//
+
+import Foundation
+
+class PhotosStore {
+    static let testPhotoNames: [String] = ["pexels-engin-akyurt-2299028",
+                                           "pexels-asad-photo-maldives-457881",
+                                           "pexels-roberto-nickson-2531237",
+                                           "pexels-darren-lawrence-3822113",
+                                           "pexels-cottonbro-4626368",
+                                           "pexels-pixabay-289998",
+                                           "pexels-breakingpic-3188",
+                                           "pexels-andrea-piacquadio-3812737",
+                                           "pexels-andrea-piacquadio-3812753",
+                                           "pexels-maud-slaats-2326936",
+                                           "pexels-martin-lopez-2530177",
+                                           "pexels-matthias-zomer-101063",
+                                           "pexels-craig-adderley-1724228",
+                                           "pexels-pok-rie-128328",
+                                           "pexels-savanna-goldring-5184327",
+                                           "pexels-pixabay-210205",
+                                           "pexels-pixabay-302831",
+                                           "pexels-andrea-piacquadio-3811324",
+                                           "pexels-life-of-pix-7640",
+                                           "pexels-pixabay-268533"]
+}
---
+++ Netology iOS homeworks/Images/PhotosTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosTableViewCell.swift
+//  Netology iOS homeworks
+//
+//  Created by Александр Шелихов on 17.09.2021.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+    
+}
